f68a14804c173d76c00ab840bd4b850066d96b0c
ChrisKnowsDev <chrisharley81@gmail.com>
2022-12-05 17:56:53 -0500

    completed about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,23 +1,74 @@
+import styled from 'styled-components'
+
 export default function About() {
   return (
-    <div>
+    <Wrapper>
+      <h1>Solving the task list challenge</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quas
-        enim harum eaque tempora ea, ratione accusamus rem voluptatibus
-        dignissimos iure molestias cupiditate sit quia commodi culpa minus. Modi
-        minima voluptatibus quam enim at. Accusamus!
+        I decided to solve this coding problem by allowing the user to click a
+        checkbox to show the task has been completed. I also provided the user
+        with a way to edit and delete a task from their list.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quia sed
-        magni esse soluta adipisci magnam beatae accusamus tempora velit!
+        I first created an add-task component which consisted of a button and a
+        controlled input. I used React’s useReducer and useContext for this
+        project. Once the user types in something to add into the input, they
+        then click the button which dispatches ‘added’ in return updates the
+        state by adding a new task. The task consist of a ID, text, and a done
+        property.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error delectus
-        distinctio officiis! Et itaque id voluptatibus debitis illum sapiente
-        eos a perferendis rerum tempore soluta suscipit minus, neque quisquam
-        doloribus, voluptatem sint, repellendus iure recusandae. Distinctio
-        voluptatibus cum magnam vero, aliquam dignissimos quisquam.
+        For the task list I created some dummy data to get started and so that I
+        always have something displayed in the task list component. The task
+        list component maps over the provided tasks from the useContext hook and
+        returns a li for each task. In the li is a task component where I pass
+        in the current task.
       </p>
-    </div>
+      <p>
+        In the task component I created state for while editing a task and
+        initially set it to false. I also created a variable to hold the content
+        of the task (taskContent) minus the checkbox since each task will always
+        show the checkbox. If isEditing is true, then the text will be an input
+        to edit the text and the edit button will say save instead of edit. If
+        isEditing is set to false then the task will just be text and the edit
+        button will of course say edit.
+      </p>
+      <p>
+        In the return of the task list component I returned the checkbox, which
+        is where the user will click if the task is complete. After the checkbox
+        is the content of the taskContent variable and then a delete button to
+        delete a task.
+      </p>
+      <p>
+        When there is a change in the input text field a dispatch ‘changed’ is
+        called passing in the action of the current task and the updated text.
+        In the reducer I map over the tasks state, find the tasks with a ID that
+        matches the passed in ID and return the passed in task. Once the user
+        clicks save the isEditing state is set to false.{' '}
+      </p>
+      <p>
+        When the checkbox detects a change (clicked or not), I call the dispatch
+        ‘changed’. This time passing in the current task and done property =
+        e.target.checked, which will update the done property to true of false
+        depending on whether the checkbox is checked or not.{' '}
+      </p>
+      Finally when the user clicks the delete button a dispatch of ‘deleted’ is
+      called passing in the task ID. In the reducer I filter over the tasks
+      state and return all the tasks except the one that has the ID of the ID
+      passed in the action
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  max-width: var(--maxWidthMed);
+  margin: 2rem auto 0;
+
+  h1 {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    padding-bottom: 1rem;
+  }
+`
