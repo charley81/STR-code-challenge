7cc7057cde21393e8d434a9873fe79d7ed816872
ChrisKnowsDev <chrisharley81@gmail.com>
2022-12-05 17:13:52 -0500

    styled completed page

--- src/components/completed-list.jsx
+++ src/components/completed-list.jsx
@@ -1,17 +1,31 @@
 import { useContext } from 'react'
 import { TasksContext } from '../context/TasksContext'
+import styled from 'styled-components'
 
 export default function CompletedList() {
   const task = useContext(TasksContext)
   console.log(task)
   return (
-    <div>
-      <h3>Completed Tasks</h3>
+    <Wrapper>
+      <h1>My Completed Tasks</h1>
       {task.map(t => {
         if (t.done === true) {
           return <li>{t.text}</li>
         }
       })}
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  max-width: var(--maxWidthMed);
+  margin: 2rem auto 0;
+
+  h1 {
+    margin-bottom: 1rem;
+  }
+
+  li {
+    margin: 1rem 0;
+  }
+`
